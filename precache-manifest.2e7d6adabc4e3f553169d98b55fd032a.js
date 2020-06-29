dfa7051315cbb0aace4ee0b4294952958acba16d
Sigurd Øines <sigurd.oines2@nav.no>
2020-06-29 13:42:55 +0200

    Updates

---
+++ precache-manifest.2e7d6adabc4e3f553169d98b55fd032a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3257672dafb0b75c8149e0a9b5ae3f7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5514d8611a205dffa0d5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "25fef6f4dcee74636a30",
+    "url": "/static/js/2.672369c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.672369c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5514d8611a205dffa0d5",
+    "url": "/static/js/main.4d7f9392.chunk.js"
+  },
+  {
+    "revision": "57ee02a10cfade742a12",
+    "url": "/static/js/runtime-main.49fe651b.js"
+  }
+]);
