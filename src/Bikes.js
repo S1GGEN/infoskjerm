83071366c3cb6bd8a8eb4c2ce0bde0af2ef5d4c9
Sigurd Øines <sigurd.oines2@nav.no>
2020-06-29 13:42:26 +0200

    Setup GitHub pages

--- src/Bikes.js
+++ src/Bikes.js
@@ -7,14 +7,11 @@ const Bikes = () => {
     const [bikeStations, setBikeStations] = useState([]);
 
     useEffect(() => {
-        //console.log("Heisann");
         service.getBikeRentalStationsByPosition(
             { latitude: 63.428311, longitude: 10.392514 },
              230
             ).then((data) => setBikeStations(data));
     }, []);
-
-    console.log(bikeStations)
 
     return <div className="bikeStations">
         <h1>Bysykkelstativ:</h1>
@@ -29,7 +26,6 @@ export default Bikes;
 const BikeStation = (props) =>  {
     const { station } = props;
 
-    console.log(station);
     return <div className="bikeStation">
         <h3>{station.name}</h3>
         <p>{station.bikesAvailable} ledige sykler</p>
